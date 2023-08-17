962f5b0f9e6a2dbe7776bc38e2bd9b566613dd10
AaronPrs <aabepaca@gmail.com>
2023-08-17 11:39:33 -0400

    add basic html and plan js execution

---
+++ script.js
@@ -0,0 +1,5 @@
+/*okay so i have to play a game of rock paper scizors
+with the computer.*/
+//for that I need to make the computer play
+//to make it understand how the game works(rock beats scizor beats paper and so on)
+//to make it play a round with the 'player'.
