697859051f3ee38a0b4e0b5761b20dc9c28ece99
AaronPrs <aabepaca@gmail.com>
2023-08-27 08:30:53 -0400

    add game function
    this needs some serious revision, so later i'll take a break and
    come back with fresh eyes to see how to solve this.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-/*okay so i have to play a game of rock paper scizors
+/*okay so i have to play a game of rock paper scissors
 with the computer.*/
 
 //for that I need to make the computer play
 //how to make the computer play,well the course says to create a function that acts as the computer
-//then what is it that the computer has to make?to choose between the options ramdonly
+//then what is it that the computer has to make?to choose between the options 
 //how do i do that?
+
+
+
 
 function getComputerChoice () {
 
@@ -25,38 +28,71 @@ function getComputerChoice () {
 
 
 //to make it play a round with the 'player'.
-//so to  make it play i create a function called playround that takes two parameters my and the computer
+//so to  make it play i create a function called play round that takes two parameters my and the computer
 /*
 it isn't working correctly,it gives different results,why?
   
  */
+let moves = 0;
 
 function playRound (playerSelection , computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
   if(playerSelection == computerSelection){
-    return "It's a tie!"}
+    alert ("It's a tie!"),
+    moves++;
+  }
     else if(playerSelection === 'rock'){
       if(computerSelection === 'paper'){
-        return 'you lose paper beats rock';
+        alert ('you lose paper beats rock'),
+        computerScore++,
+        moves++;
     }else{
-      return 'you win rock beats scissors'}
+      alert ('you win rock beats scissors'),
+      playerScore++,
+      moves++}
     }else if(playerSelection === 'scissors'){
       if(computer === 'rock'){
-          return 'you lose rock beats scissors';
+          alert ('you lose rock beats scissors'),
+          computerScore++,
+          moves++;
       }else{
-          return 'you win scissors beats paper';
+          alert ('you win scissors beats paper'),
+          playerScore++,
+          moves++;
       }
     }else if(playerSelection === 'paper'){
       if(computerSelection === 'scissors'){
-          return 'you lose scissors beats paper';
+          alert ( 'you lose scissors beats paper'),
+          computerScore++,
+          moves++;
       }else{
-          return 'you win paper beats rock';
+          alert ('you win paper beats rock'),
+          playerScore++,
+          moves++;
       }    
     }     
 }
 
-const playerSelection = "rock";
+const playerSelection = prompt();
 const computerSelection = getComputerChoice();
+
+
+
+
+function game(playerScore, computerScore,){
+for (playerScore  || computerScore  ; playerScore !== 5 || computerScore !== 5 ; playRound ){
+  if (playerScore === 5 || computerScore === 5) {
+    const winner =
+      playerScore === 5
+        ? "You win the game! Congratulations!"
+        : "Computer wins the game! Try again next time!";
+    alert (winner);
+  }
+}
+}
+
+
 console.log(computerSelection)
 console.log(playRound(playerSelection, computerSelection));
+console.log(game(playerScore, computerScore));
